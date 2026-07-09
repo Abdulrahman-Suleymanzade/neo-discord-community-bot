c9d5853bc7871de675e1c3444ad3c7b1f31de36d
Abdulrahman Suleymanzade <sightseeing6767@gmail.com>
2026-07-09 16:27:56 -0400

    Add prestige calculation functions

--- leveling.py
+++ leveling.py
@@ -20,3 +20,14 @@ def xp_for_next_level(level: int) -> int:
 
 def xp_for_level(level: int) -> int:
     return (level ** 2) * XP_MULTIPLIER
+
+def calculate_prestige(level: int) -> int:
+    if level < 100:
+        return 0
+    return ((level - 100) // 50) + 1
+
+
+def next_prestige_level(level: int) -> int:
+    if level < 100:
+        return 100
+    return 100 + (calculate_prestige(level) * 50)
